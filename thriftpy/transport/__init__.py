676f21444e443e5ee842b7c475d94dc4372286f1
Martin Durant <martin.durant@utoronto.ca>
2017-04-19 14:09:56 -0400

    Gut library
    Only including pieces used by fastparquet

--- thriftpy/transport/__init__.py
+++ thriftpy/transport/__init__.py
@@ -2,24 +2,11 @@
 
 from __future__ import absolute_import
 
-from thriftpy._compat import CYTHON
-
 from ..thrift import TType, TException
 
 
 def readall(read_fn, sz):
-    buff = b''
-    have = 0
-    while have < sz:
-        chunk = read_fn(sz - have)
-        have += len(chunk)
-        buff += chunk
-
-        if len(chunk) == 0:
-            raise TTransportException(TTransportException.END_OF_FILE,
-                                      "End of file reading from transport")
-
-    return buff
+    return read_fn.read(sz)
 
 
 class TTransportBase(object):
@@ -52,39 +39,4 @@ class TTransportException(TException):
         self.message = message
 
 
-# Avoid recursive import
-from .socket import TSocket, TServerSocket  # noqa
-from .sslsocket import TSSLSocket, TSSLServerSocket  # noqa
-from ._ssl import create_thriftpy_context  # noqa
-from .buffered import TBufferedTransport, TBufferedTransportFactory  # noqa
-from .framed import TFramedTransport, TFramedTransportFactory  # noqa
-from .memory import TMemoryBuffer  # noqa
-
-if CYTHON:
-    from .buffered import TCyBufferedTransport, TCyBufferedTransportFactory
-    from .framed import TCyFramedTransport, TCyFramedTransportFactory
-    from .memory import TCyMemoryBuffer
-
-    # enable cython binary by default for CPython.
-    TMemoryBuffer = TCyMemoryBuffer  # noqa
-    TBufferedTransport = TCyBufferedTransport  # noqa
-    TBufferedTransportFactory = TCyBufferedTransportFactory  # noqa
-    TFramedTransport = TCyFramedTransport  # noqa
-    TFramedTransportFactory = TCyFramedTransportFactory  # noqa
-else:
-    # disable cython binary protocol for PYPY since it's slower.
-    TCyMemoryBuffer = TMemoryBuffer
-    TCyBufferedTransport = TBufferedTransport
-    TCyBufferedTransportFactory = TBufferedTransportFactory
-    TCyFramedTransport = TFramedTransport
-    TCyFramedTransportFactory = TFramedTransportFactory
-
-__all__ = [
-    "TSocket", "TServerSocket",
-    "TSSLSocket", "TSSLServerSocket", "create_thriftpy_context",
-    "TTransportBase", "TTransportException",
-    "TMemoryBuffer", "TFramedTransport", "TFramedTransportFactory",
-    "TBufferedTransport", "TBufferedTransportFactory", "TCyMemoryBuffer",
-    "TCyBufferedTransport", "TCyBufferedTransportFactory",
-    "TCyFramedTransport", "TCyFramedTransportFactory"
-]
+__all__ = ["TTransportBase", "TTransportException"]
